7e83b27a10cbf9abb1df38d23dfa403bc9f8c032
ariannadrovandi <ariannadrovandi02@gmail.com>
2023-03-21 17:17:10 +0100

    added numGenerator

---
+++ js/script.js
@@ -0,0 +1,33 @@
+/*
+Descrizione:
+Visualizzare in pagina 5 numeri casuali. Da lì parte un timer di 30 secondi.
+Dopo 30 secondi i numeri scompaiono e l'utente deve inserire, i numeri 
+che ha visto precedentemente.
+Dopo che sono stati inseriti i 5 numeri, il software dice quanti e quali 
+dei numeri da indovinare sono stati individuati.
+*/
+
+/*
+mostro la pagina con il titolo e il btn start,
+al click genero i numeri casuali e li mostro a schermo,
+creo il countdown e lo faccio partire,
+quando arriva a 0 nascondo i numeri casuali, 
+mostro l'istruzione successiva e la text area per la risposta
+
+
+
+*/
+
+//funzione che genera i numeri
+function numGenerator(){
+    const rdnNumbers=[]; 
+    while (rdnNumbers.length <= 4){
+        let rdnNumber = getRndNumIncl(1, 50);
+        if(!rdnNumbers.includes(rdnNumber)){
+            rdnNumbers.push(rdnNumber);
+        }
+    }
+    return rdnNumbers
+}
+
+
--- js/utility.js
+++ js/utility.js
@@ -2,13 +2,16 @@ function getRandomInt(min, max) {
     return Math.floor(Math.random() * (max - min + 1)) + min;
 } 
 
-
+//Il massimo è escluso e il minimo è incluso
 function getRndNum(min, max) {
     return Math.floor(Math.random() * (max - min) ) + min;
 }
 
+// Il massimo è incluso e il minimo è incluso
 function getRndNumIncl(min, max) {
     min = Math.ceil(min);
     max = Math.floor(max);
     return Math.floor(Math.random() * (max - min + 1) + min); 
 }
+
+
